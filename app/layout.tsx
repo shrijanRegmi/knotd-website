9ebf9602b6b6bdbc58794bf0d574a40db85ac358
Shrijan Regmi <your.email@example.com>
2026-03-04 23:25:48 +0545

    feat: added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import Providers from "@/app/components/Providers";
 import "./globals.css";
 
@@ -23,6 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+        )}
         <Providers>{children}</Providers>
       </body>
     </html>
